649c83863f49a968ebd0e4e02b579d5a854bc830
Nicolaj Linde <nicolajlpedersen@gmail.com>
2022-09-11 12:45:15 +0200

    Finished the game! Yay!

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Quitting the game!");
+            Application.Quit();
+        }
+    }
+}
